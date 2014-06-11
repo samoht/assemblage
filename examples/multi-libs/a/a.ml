b073652a0a7b794d3fc8a672c6abdf4d2c05e125
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-06-11 18:36:55 +0100

    Add an example of a project with multiple local libraries

---
+++ examples/multi-libs/a/a.ml
@@ -0,0 +1,1 @@
+let x = Ezjsonm.from_string "{}"
---
+++ examples/multi-libs/make.ml
@@ -0,0 +1,14 @@
+#use "../../lib/makefile.ml"
+
+let a =
+  Library.create ~dir:"a"
+    [ Unit.create ~deps:[Depend.library "ezjsonm"] "a"]
+    "lib1"
+
+let b =
+  Library.create ~dir:"b"
+    [ Unit.create ~deps:[Depend.local a] "b"]
+    "lib2"
+
+let () =
+  generate (ocaml [a; b])
