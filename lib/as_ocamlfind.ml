21ca097961b0e76d92c85c944dd49372b82797cc
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-08-12 16:58:02 +0100

    Fix generation of META dependencies
    Previously, only the library dependencies were taken into account, and not the individual compilation unit ones.

--- lib/as_ocamlfind.ml
+++ lib/as_ocamlfind.ml
@@ -18,8 +18,8 @@ open Printf
 module StringSet = Set.Make (String)
 
 let (/) = Filename.concat
-
 let (|>) x f = f x
+let conmap f l = List.concat (List.map f l)
 
 type mode = [`Direct|`Indirect|`Makefile]
 
@@ -146,8 +146,10 @@ module META = struct
     let version = As_project.version t in
     let buf = Buffer.create 1024 in
     let one lib =
+      let c = `Lib lib in
       let requires =
-        As_project.Lib.deps lib
+        conmap
+          As_project.Component.deps (c :: As_project.Component.contents c)
         |> As_project.Component.closure
         |> As_project.Component.(filter pkg_ocaml)
         |> List.map As_project.Pkg.name
