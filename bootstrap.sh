7ea65719a1acd56b64a9574a7ca7e69e78f8aa77
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-08-12 17:00:45 +0100

    Move away from enable/disable command line flags, use --name=BOOL.
    Fixes #73.

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,5 +42,5 @@ $OCAMLFIND ocamlc $OPTS $UPKGS -I $BDIR -linkpkg $CMOS $BDIR/tool.cmo \
     -o $BDIR/assemblage.boot
 
 # Run it on assemblage's assemblage.ml
-$BDIR/assemblage.boot setup --disable-auto-load -I $BDIR \
-    --enable-warn-error --disable-test
+$BDIR/assemblage.boot setup --auto-load=false -I $BDIR \
+    --warn-error=false --test=false
