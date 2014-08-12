e86b8e5bb84a38c454c7fedbc4e7d22ba0d4a8e3
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-08-12 22:39:41 +0100

    Fix bootstrap: optcomp is not required anymore

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ OCAMLFIND=${OCAMLFIND:="ocamlfind"}
 BDIR="_build/bootstrap"
 LIBDIR="lib"
 PKGS="-package cmdliner,ocamlgraph"
-OPTCOMP="-syntax camlp4o -package camlp4,optcomp"
 
 UNITS="as_shell as_git as_features as_flags as_resolver as_action
        as_build_env as_project as_ocamlfind as_OCaml_incl as_OCaml
@@ -27,7 +26,7 @@ if [ $major -ge 4 ] && [ $xminor -le 01 ]; then minor=01; else minor=$xminor; fi
 for u in $UNITS; do
     case $u in
     "as_OCaml_incl") f=$major$minor/$u; UPKGS="$PKGS,compiler-libs.bytecomp"; OPTS="" ;;
-    "as_OCaml") f="$u"; UPKGS="$PKGS,compiler-libs.bytecomp"; OPTS="$OPTS $OPTCOMP" ;;
+    "as_OCaml") f="$u"; UPKGS="$PKGS,compiler-libs.bytecomp"; OPTS="$OPTS" ;;
     *)          f="$u"; UPKGS="$PKGS"; OPTS="" ;;
     esac
 
